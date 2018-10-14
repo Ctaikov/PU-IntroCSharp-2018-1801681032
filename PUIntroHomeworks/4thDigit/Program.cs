e7013bdb43f65b7e6a017a7a14d3f2a649e03422
Denislav Staikov <denislavw3@gmail.com>
2018-10-14 15:58:44 +0300

    Included homework for week two.

---
+++ PUIntroHomeworks/4thDigit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4thDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a 7 digit number:");
+            int number = int.Parse(Console.ReadLine());
+            if (number<1000000 || number>9999999)
+            {
+                Console.WriteLine("Your number is not 7 digits long.");
+            }
+            else
+            {
+                int fourthDigit = number % 1000000 % 100000 % 10000 / 1000;
+                Console.WriteLine($"The 4th digit of {number} is {fourthDigit}.");
+            }
+
+
+        }
+    }
+}
---
+++ PUIntroHomeworks/DecartCoordinates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DecartCoordinates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time to do some math.");
+            Console.WriteLine("------------------------------");
+            Console.Write("Enter Xa: ");
+            double Xa = double.Parse(Console.ReadLine());
+            Console.Write("Enter Ya: ");
+            double Ya = double.Parse(Console.ReadLine());
+            Console.Write("Enter Xb: ");
+            double Xb = double.Parse(Console.ReadLine());
+            Console.Write("Enter Yb: ");
+            double Yb = double.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("The points have these coordinates:");
+            Console.WriteLine($"Point A({Xa}, {Ya}).");
+            Console.WriteLine($"Point B({Xb}, {Yb}).");
+            Console.WriteLine("------------------------------");
+            double D = Math.Sqrt(Math.Pow(Xb-Xa,2)+Math.Pow(Yb-Ya,2));
+            Console.WriteLine($"The distance between the two points is {D}.");
+
+        }
+    }
+}
