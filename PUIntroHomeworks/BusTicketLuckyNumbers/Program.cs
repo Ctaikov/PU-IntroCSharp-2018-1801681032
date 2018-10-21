525268bb532b68a4c11c015c2cb8cb0a77cf0a0c
Denislav Staikov <denislavw3@gmail.com>
2018-10-21 17:11:14 +0300

    Included homework for week three.

---
+++ PUIntroHomeworks/BusTicketLuckyNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BusTicketLuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luckyNumbersCount = 0;
+            int unluckyNumberCount = 0;
+            for (int i=0; i<1000000;i++)
+            {
+                if((i / 100000 + i % 100000 / 10000 + i % 100000 % 10000 / 1000)==(i % 100000 % 10000 % 1000 / 100 + i % 100000 % 10000 % 1000 % 100 / 10 + i % 10))
+                {
+                    Console.WriteLine(i);
+                    luckyNumbersCount++;
+                }
+                else
+                {
+                    unluckyNumberCount++;
+                }
+            }
+            Console.WriteLine($"The total number of ticket is {luckyNumbersCount+unluckyNumberCount}.");
+            Console.WriteLine($"The total number of lucky tickets is {luckyNumbersCount}.");
+            Console.WriteLine($"The total number of unlucky tickets is {unluckyNumberCount}.");
+        }
+    }
+}
