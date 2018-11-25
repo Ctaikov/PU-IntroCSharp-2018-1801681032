0791a6534842bff11ca449d44c581b252ef95655
Denislav Staikov <denislavw3@gmail.com>
2018-11-25 21:08:57 +0200

    Included homework for week eight.

---
+++ PUIntroHomeworks/AverageNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int evenNumberCount=0;
+            double evenNumberSum=0;
+            do
+            {
+                Console.WriteLine("Enter a number between 1 and 255:");
+                number = int.Parse(Console.ReadLine());
+                if ((number>=1 && number<256) && number%2==0)
+                {
+                    evenNumberSum += number;
+                    evenNumberCount++;
+                }
+            } while (number!=0);
+            if(evenNumberCount==0)
+            {
+                Console.WriteLine("Please, enter a number between 1 and 255.");
+            }
+            else
+            {
+                Console.WriteLine($"Average {evenNumberSum / evenNumberCount}.");
+            }
+        }
+    }
+}
---
+++ PUIntroHomeworks/CircleArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            double radius = rand.Next(33, 187);
+            double circleArea=Math.PI*(Math.Pow(radius,2));
+            Console.WriteLine($"The circle's random radius is {radius}.");
+            Console.WriteLine($"The circle's area is: {Math.Round(circleArea,2)}.");
+        }
+    }
+}
